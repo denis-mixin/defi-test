92851b9cf272d3634b60408e527e5e381379153b
Denisn <denis.nikitin@gotech.io>
2022-01-19 14:29:45 +0200

    Updates

---
+++ assets/js/images.js
@@ -0,0 +1,40 @@
+
+const images = {
+    defiLogoWithName: '/assets/images/main/defi-logo-with-name.svg',
+    orbsLogoWithName: '/assets/images/main/orbs-logo-with-name.svg',
+    globeFigure: '/assets/images/main/globe-figure.png',
+    defiNotifyLogo: '/assets/images/main/defi-notify-logo.svg',
+    arrowRight: '/assets/images/main/arrow-right.svg',
+    bell: '/assets/images/main/bell.png',
+    publicBeta: '/assets/images/main/public-beta.svg',
+    download: '/assets/images/main/download.svg',
+    apple: '/assets/images/main/apple.svg',
+    google: '/assets/images/main/google.svg',
+    play: '/assets/images/main/play.svg',
+    sushiLink: '/assets/images/main/sushi-link.png',
+    aaveLink: '/assets/images/main/aave-link.png',
+   
+    orbsLogo: '/assets/images/main/orbs-logo.png',
+    
+    
+    heroShadow: '/assets/images/main/hero-shadow.png',
+    mobile: {
+        globe: '/assets/images/main/mobile/globe.png',
+    },
+    videos: {
+        mobileShadow: '/assets/images/main/videos/mobile-shadow.png',
+        shadow: '/assets/images/main/videos/shadow.png',
+    },
+    footer: {
+        shadow: '/assets/images/main/footer/shadow.png',
+        mobileShadow: '/assets/images/main/footer/mobile-shadow.png',
+    },
+    media:{
+        yahoo: '/assets/images/main/media/yahoo.png',
+        shadow: '/assets/images/main/media/shadow.png',
+       
+    }
+}
+
+
+export default images
